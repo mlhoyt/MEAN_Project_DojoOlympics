141159612522e022d8d34060df2308e5bf8fceec
Mike Hoyt <derukimlh@gmail.com>
2017-06-27 14:42:19 -0700

    Added Question model and added/updated related files

--- server/config/routes.js
+++ server/config/routes.js
@@ -31,6 +31,14 @@ module.exports = function( globals ) {
   globals.app.put    ( '/api/exams/:pk', ( req, res ) => Exam_ctrlr.update   ( req, res ) );
   globals.app.delete ( '/api/exams/:pk', ( req, res ) => Exam_ctrlr.delete   ( req, res ) );
 
+  let Question_ctrlr = require( '../controllers/Question.js' );
+
+  globals.app.post   ( '/api/questions',     ( req, res ) => Question_ctrlr.create   ( req, res ) );
+  globals.app.get    ( '/api/questions',     ( req, res ) => Question_ctrlr.read_all ( req, res ) );
+  globals.app.get    ( '/api/questions/:pk', ( req, res ) => Question_ctrlr.read_one ( req, res ) );
+  globals.app.put    ( '/api/questions/:pk', ( req, res ) => Question_ctrlr.update   ( req, res ) );
+  globals.app.delete ( '/api/questions/:pk', ( req, res ) => Question_ctrlr.delete   ( req, res ) );
+
   // Default (delegate to front-end router)
   globals.app.all( '*', ( req, res ) => res.sendFile( path.resolve( './client/dist/index.html' ) ) );
 }
---
+++ server/controllers/Question.js
@@ -0,0 +1,43 @@
+// -*- javascript -*-
+
+let mongoose = require('mongoose');
+
+let Question = mongoose.model( 'Question' );
+
+module.exports = {
+  create: function( req, res ) {
+    let item = new Question( req.body );
+    item.save()
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( () => res.json( true ) );
+  },
+
+  read_all: function( req, res ) {
+    Question.find({})
+      .populate('category')
+      .populate('topic')
+      // .sort( '{{FIELD_NAME}}|-{{FIELD_NAME}}') // createdAt, -createdAt
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( data => res.json( data ) );
+  },
+
+  read_one: function( req, res ) {
+    Question.findOne({ _id: req.params.pk })
+      .populate('category')
+      .populate('topic')
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( data => res.json( data ) );
+  },
+
+  update: function( req, res ) {
+    Question.updateOne({ _id: req.params.pk }, {$set: req.body})
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( () => res.json( true ) );
+  },
+
+  delete: function( req, res ) {
+    Question.remove({ _id: req.params.pk })
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( () => res.json( true ) );
+  },
+}
