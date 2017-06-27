26929a3c5c41cc7ec113a0155a383c2262bfda6c
Mike Hoyt <derukimlh@gmail.com>
2017-06-27 08:02:47 -0700

    Updated server/models/Category.js - added minor validation constraint: minlength = '1'

--- server/models/Category.js
+++ server/models/Category.js
@@ -11,42 +11,8 @@ module.exports = function( globals ) {
         type: String,
         index: true,
         required: true,
+        minlength: 1,
       },
-      // Unique Field/s
-      // {{FIELD_NAME}}: {
-      //   type: {{String, Number, Boolean, Date, Mixed=>{}, Array=>[<TYPE>], ObjectId=>Schema.Types.ObjectId}},
-      //   required: {{true, false}}, ]  # -or- [ {{true, false}}, '{{MESSAGE}}'
-      //   unique: true,
-      //   minlength: {{Number}} },
-      //   maxlength: {{Number}} },
-      //   min: {{Number|new Date('YYYY-MM-DD')}} },
-      //   max: {{Number|new Date('YYYY-MM-DD')}} },
-      //   enum: globals.{{DATA}}_TYPES,
-      //   trim: true,
-      //   validate: [
-      //     {
-      //       validator: function( value ) {
-      //         return ...; # boolean /{{REGEX}}/.test( value );
-      //       },
-      //       message: '...{ VALUE }...'
-      //     },
-      //     ...
-      //   ],
-      // },
-      // ...
-
-      // Many-to-* Relationship/s
-      // _{{FIELD_NAME}}: [{
-      //   type: Schema.Types.ObjectId, ref: '',
-      //   default: [],
-      // }],
-      // ...
-
-      // One-to-* Relationship/s
-      // _{{FIELD_NAME}}: {
-      //   type: Schema.Types.ObjectId, ref: '',
-      // },
-      // ...
     },
     {
       timestamps: true,
