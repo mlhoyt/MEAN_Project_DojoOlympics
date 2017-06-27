b74bad2c290276cbdbf5f703e960b21a9552817f
Mike Hoyt <derukimlh@gmail.com>
2017-06-27 08:28:40 -0700

    Added Exam model and added/updated related files

--- server/config/routes.js
+++ server/config/routes.js
@@ -23,6 +23,14 @@ module.exports = function( globals ) {
   globals.app.put    ( '/api/topics/:pk', ( req, res ) => Topic_ctrlr.update   ( req, res ) );
   globals.app.delete ( '/api/topics/:pk', ( req, res ) => Topic_ctrlr.delete   ( req, res ) );
 
+  let Exam_ctrlr = require( '../controllers/Exam.js' );
+
+  globals.app.post   ( '/api/exams',     ( req, res ) => Exam_ctrlr.create   ( req, res ) );
+  globals.app.get    ( '/api/exams',     ( req, res ) => Exam_ctrlr.read_all ( req, res ) );
+  globals.app.get    ( '/api/exams/:pk', ( req, res ) => Exam_ctrlr.read_one ( req, res ) );
+  globals.app.put    ( '/api/exams/:pk', ( req, res ) => Exam_ctrlr.update   ( req, res ) );
+  globals.app.delete ( '/api/exams/:pk', ( req, res ) => Exam_ctrlr.delete   ( req, res ) );
+
   // Default (delegate to front-end router)
   globals.app.all( '*', ( req, res ) => res.sendFile( path.resolve( './client/dist/index.html' ) ) );
 }
---
+++ server/controllers/Exam.js
@@ -0,0 +1,39 @@
+// -*- javascript -*-
+
+let mongoose = require('mongoose');
+
+let Exam = mongoose.model( 'Exam' );
+
+module.exports = {
+  create: function( req, res ) {
+    let item = new Exam( req.body );
+    item.save()
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( () => res.json( true ) );
+  },
+
+  read_all: function( req, res ) {
+    Exam.find({})
+      // .sort( '{{FIELD_NAME}}|-{{FIELD_NAME}}') // createdAt, -createdAt
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( data => res.json( data ) );
+  },
+
+  read_one: function( req, res ) {
+    Exam.findOne({ _id: req.params.pk })
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( data => res.json( data ) );
+  },
+
+  update: function( req, res ) {
+    Exam.updateOne({ _id: req.params.pk }, {$set: req.body})
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( () => res.json( true ) );
+  },
+
+  delete: function( req, res ) {
+    Exam.remove({ _id: req.params.pk })
+      .catch( err => res.status( 500 ).json( err ) )
+      .then( () => res.json( true ) );
+  },
+}
