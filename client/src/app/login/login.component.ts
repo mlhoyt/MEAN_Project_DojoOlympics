f68a00010f384bc74cf6b8dc32d0eb2648c4167b
Mike Hoyt <derukimlh@gmail.com>
2017-06-28 12:53:02 -0700

    Added new_user socket event from client to server

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../socket.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user_name = "";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private _socket: SocketService,
+  )
+  {
   }
 
+  ngOnInit() {
+    // this._socket.connect();
+  }
+
+  doSendUserName() {
+    if( this.user_name != "" ) {
+      console.log( "Debug: LoginComponent: doSendUserName: sending socket event 'new_user'" );
+      this._socket.new_user( { user_name: this.user_name } );
+    }
+    else {
+      console.log( "Debug: LoginComponent: doSendUserName: ignoring click event with empty user_name" );
+    }
+  }
 }
